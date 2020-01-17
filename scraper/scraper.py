623293ee4886de1c7f00f167e719be3e1fb95810
Gilang R Ilhami <gigilangilhami@gmail.com>
2020-01-17 11:41:51 +0700

    Completed detik scraper
    Completed DetikScraper class parse
    method.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,6 @@
 import scrapy
 import time
+import datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,10 +9,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-from .utils import retrieve_date_year
+from utils import retrieve_date_year
 
 FIRST_YEAR = 2017
 SECOND_YEAR = 2018
+KEYWORD = "tenaga nuklir indonesia"
 
 class DetikScraper(scrapy.Spider):
     name = "detik_scraper"
@@ -20,25 +22,25 @@ class DetikScraper(scrapy.Spider):
 
     def __init__(self):
         chromedriver_path = "/mnt/c/Users/Gilang R Ilhami/Desktop/personal_projects/ibm_stuff/chromedriver.exe"
-        self.driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+        # self.driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
 
         # Commend code above, and 
         # uncomment code below
         # to remove headless scraping.
-        # driver = webdriver.Chrome(executable_path=chromedriver_path)
+        self.driver = webdriver.Chrome(executable_path=chromedriver_path)
 
     def parse(self, response):
 
-        url = reponse.url
+        url = response.url
         self.driver.get(url)
         self.driver.implicitly_wait(20)
 
         self.driver.implicitly_wait(10)
         input_search = self.driver.find_element_by_xpath("//*[@id='search_navbar']/input[1]")
-        input_search.send_keys(keyword.lower())
+        input_search.send_keys(KEYWORD.lower())
         input_search.send_keys(Keys.RETURN)
 
-        driver.implicitly_wait(10)
+        self.driver.implicitly_wait(10)
         news_list = self.driver.find_element_by_xpath("/html/body/div[2]/div/div[2]")
 
         link_list = news_list.find_elements_by_tag_name("a")
@@ -55,18 +57,137 @@ class DetikScraper(scrapy.Spider):
 
         # Get the latest year from the latest news
         for _ in range(len(link_list)):
+
             try:
-                last_news_year = retrieve_date_year(link_list[last_idx], "div", "class", "date")
+                latest_news_year = retrieve_date_year(link_list[last_idx], "div", "class", "date")
                 print(f"Found latest year at index {last_idx}")
-                print(f"Latest year is {last_news_year}")
+                print(f"Latest year is {latest_news_year}")
                 break
-            except Exception as e:
-                print(f"Can't find latest year with index {last_idx}")
+            except:
+
+                try:
+                    latest_news_year = retrieve_date_year(link_list[last_idx], "div", "class", "detail__date")
+                    print(f"Found latest year at index {last_idx}")
+                    print(f"Latest year is {latest_news_year}")
+                    break
+                except Exception as e:
+                    print(f"Can't find latest year with index {last_idx}: '{e}'")
+
             last_idx -= 1
 
         
-        if last_news_year < FIRST_YEAR:
-            all_link_list = link_list
+        if latest_news_year < FIRST_YEAR:
+            all_links = link_list
         else:
-            all_link_list = []
+            all_links = []
 
+        # While the latest news larger than
+        # the first year, retrieve the url.
+        while latest_news_year >= FIRST_YEAR:
+
+            print("="*20)
+            print("Retrieving all the news url...")
+            print("="*20)
+            # Wait to load page
+            time.sleep(5)
+
+            self.driver.implicitly_wait(10)
+            news_list = self.driver.find_element_by_xpath("/html/body/div[2]/div/div[2]")
+
+            link_list = news_list.find_elements_by_tag_name("a")
+
+            link_not_none = lambda link: link.get_attribute("href") is not None
+            link_list = [link.get_attribute("href") for link in link_list if link_not_none]
+
+            all_links.extend(link_list)
+
+            last_idx = -1
+
+            for _ in range(len(link_list)):
+                try:
+                    latest_news_year = retrieve_date_year(link_list[last_idx], "div", "class", "date")
+                    print(f"Found latest year at index {last_idx}")
+                    print(f"Latest year is {latest_news_year}")
+                    break
+                except:
+                    try:
+                        lastest_news_year = retrieve_date_year(link_list[last_idx], "div", "class", "detail__date")
+                        print(f"Found latest year at index {last_idx}")
+                        print(f"Latest year is {latest_news_year}")
+                        break
+                    except Exception as e:
+                        print(f"Can't find latest year with index {last_idx}: '{e}'")
+                last_idx -= 1
+            
+            try:
+
+                pages = self.driver.find_element_by_tag_name("div[class*='paging text_center']")
+                current_page = pages.find_element_by_tag_name("a[class*='selected']")
+                current_page_num = int(current_page.text)
+                current_page_link = current_page.get_attribute("href")
+
+            except Exception as e:
+
+                print("Something went wrong while going to next page.")
+                print(e)
+                
+                print("Going back")
+                self.driver.execute_script("window.history.go(-1)")
+                
+                print("Skipping next page")
+                next_next_page = current_page_num + 2
+                next_next_link = current_page_link[:-1] + str(next_next_page)
+                self.driver.get(next_next_link)
+
+            else:
+
+                next_page = pages.find_elements_by_tag_name("a")[-1]
+                next_page.click()
+
+        links = all_links.copy()
+
+        first_year_contents = []
+        second_year_contetns = []
+
+        all_index = set(map(links.index, links))
+
+        error_dates_index = set()
+
+        for link in links:
+            try:
+                date = retrieve_date_year(link,"div", "class", "date")
+            except:
+                try:
+                    date = retrieve_date_year(link,"div", "class", "detail__date")
+                except Exception as e:
+                    print(f"Cannot get content from {link}. {e}\n\n")
+                    error_idx = links.index(link)
+                    error_dates_index.add(error_idx)
+        
+        can_get_dates_index = list(all_index - error_dates_index)
+
+        current_date = datetime.datetime.now()
+        current_year = current_date.year
+
+        for content_index in can_get_dates_index:
+
+            # News Publish Date
+            try:
+                content_date = retrieve_date_year(links[content_index],"div", "class", "date")
+            except:
+                content_date = retrieve_date_year(links[content_index],"div", "class", "detail__date")
+            print(content_date)
+
+            # Seperating First Year and Second Year contents
+            if content_date >= FIRST_YEAR and content_date < SECOND_YEAR:
+                first_year_contents.append(links[content_index])
+            elif content_date >= SECOND_YEAR and content_date < current_year:
+                second_year_contetns.append(links[content_index])
+            else:
+                pass
+                
+        yield {
+            f"{FIRST_YEAR} contents":first_year_contents,
+            f"{SECOND_YEAR} contents": second_year_contetns
+
+        }
