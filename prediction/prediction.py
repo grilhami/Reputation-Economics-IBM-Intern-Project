78fce8a640690c762fd5a4dc4fe60ba9f6e2ea79
Gilang R Ilhami <gigilangilhami@gmail.com>
2020-02-27 20:21:06 +0700

    Add personality insight function
    Create a function for personality insight prediction.
    
    - Add a function named 'get_self_expression' in
      prediction.py for personality insight prediction.
    - Add a folder that contains sample data for prediction.

--- prediction/prediction.py
+++ prediction/prediction.py
@@ -1,9 +1,11 @@
 import argparse
+import json
 import requests
 
 from bs4 import BeautifulSoup
-from configparser import Configparser
+from configparser import ConfigParser
 from io import BytesIO
+from tika import parser
 
 from translate_selenium import Translate
 
@@ -11,22 +13,22 @@ from ibm_botocore.client import Config, ClientError
 from ibm_watson import PersonalityInsightsV3, DiscoveryV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-config = Configparser()
+config = ConfigParser()
 config.read("prediction.conf")
 
-parser = argparse.ArgumentParser(description='Provide required files for prediction.')
-parser.add_argument('--path-cxo-pdf-file', type=str,
-                    help="path to the pdf file that contains CXO's statement (e.g. annual report)")
-parser.add_argument('--path-news-list', type=str,
-                    help='path to txt file that contains list of news')
-parser.add_argument('--company-profitability', type=float,
-                    help="Profitability of a company from annual report.")
+# parser = argparse.ArgumentParser(description='Provide required files for prediction.')
+# parser.add_argument('--path-cxo-pdf-file', type=str,
+#                     help="path to the pdf file that contains CXO's statement (e.g. annual report)")
+# parser.add_argument('--path-news-list', type=str,
+#                     help='path to txt file that contains list of news')
+# parser.add_argument('--company-profitability', type=float,
+#                     help="Profitability of a company from annual report.")
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
-PATH_TO_CXO_STATEMENT = args['path-cxo-pdf-file']
-PATH_TO_NEWS_LSIT = args['path-news-list']
-PROFITABILITY = args['company-profitability']
+# PATH_TO_CXO_STATEMENT = args['path-cxo-pdf-file']
+# PATH_TO_NEWS_LSIT = args['path-news-list']
+# PROFITABILITY = args['company-profitability']
 
 PERSONALITY_INSIGHT_AUTHENTICATOR = config['personality-insight']['apikey']
 PERSONALITY_INSIGHT_SERVICE_URL = config['personality-insight']['url']
@@ -53,8 +55,19 @@ DiscoveryService.set_service_url(DISCOVERY_SERVICE_URL)
 
 def get_self_expresion(filepath):
     
-    data = open(filepath, "r")
+    content = parser.from_file(filepath)['content']
+    profile = personality_insights.profile(
+            content,
+            accept="application/json",
+            content_type='text/plain;charset=utf-8',
+            consumption_preferences=False,
+            raw_scores=False).get_result()
 
+    needs = profile['needs']
+    needs_percentile = lambda x: True if needs[x]['trait_id'] == 'need_self_expression' else False
 
+    percentile, = [needs[i]['percentile'] for i in range(len(needs)) if needs_percentile(i)]
+
+    return percentile
 
 
