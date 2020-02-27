fde7876cd5fca293ffb2808d2f2be63dd574e03f
Gilang R Ilhami <gigilangilhami@gmail.com>
2020-02-27 13:06:55 +0700

    Modified script for prediction
    Made some moficcation on making prediction.
    
    - Add parser for configuration and command
      line arguments in prediction.py
    - Include IBM Cloud credentials in prediction.py
    - Remove actual credentials and add file
      prediction.conf.sample

--- prediction/prediction.py
+++ prediction/prediction.py
@@ -1,3 +1,4 @@
+import argparse
 import requests
 
 from bs4 import BeautifulSoup
@@ -12,3 +13,48 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 config = Configparser()
 config.read("prediction.conf")
+
+parser = argparse.ArgumentParser(description='Provide required files for prediction.')
+parser.add_argument('--path-cxo-pdf-file', type=str,
+                    help="path to the pdf file that contains CXO's statement (e.g. annual report)")
+parser.add_argument('--path-news-list', type=str,
+                    help='path to txt file that contains list of news')
+parser.add_argument('--company-profitability', type=float,
+                    help="Profitability of a company from annual report.")
+
+args = parser.parse_args()
+
+PATH_TO_CXO_STATEMENT = args['path-cxo-pdf-file']
+PATH_TO_NEWS_LSIT = args['path-news-list']
+PROFITABILITY = args['company-profitability']
+
+PERSONALITY_INSIGHT_AUTHENTICATOR = config['personality-insight']['apikey']
+PERSONALITY_INSIGHT_SERVICE_URL = config['personality-insight']['url']
+DISCOVERY_AUTHENTICATOR = config['discovery']['apikey']
+DISCOVERY_SERVICE_URL = config['discovery']['url']
+
+
+# Personality Insight Authenticator
+authenticator = IAMAuthenticator(PERSONALITY_INSIGHT_AUTHENTICATOR)
+personality_insights = PersonalityInsightsV3(
+    version='2017-10-13',
+    authenticator=authenticator
+)
+personality_insights.set_service_url(PERSONALITY_INSIGHT_SERVICE_URL)
+
+# Discovery Authenticator
+authenticator = IAMAuthenticator(DISCOVERY_AUTHENTICATOR)
+DiscoveryService = DiscoveryV1(
+    version='2019-11-22',
+    authenticator=authenticator
+)
+DiscoveryService.set_service_url(DISCOVERY_SERVICE_URL)
+
+
+def get_self_expresion(filepath):
+    
+    data = open(filepath, "r")
+
+
+
+
